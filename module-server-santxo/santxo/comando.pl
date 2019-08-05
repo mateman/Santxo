f96ab290d577f3fb3990bb6bc985e370418c3e5c
mateman <cuyeu@hotmail.com>
2019-08-05 14:06:09 -0300

    Update comando.pl

--- module-server-santxo/santxo/comando.pl
+++ module-server-santxo/santxo/comando.pl
@@ -2,8 +2,6 @@
 use Socket;
 my $i = $ARGV[0];
 my $p = $ARGV[1];
-#my $pid = fork();
-#if (defined $pid && $pid==0){
 
 socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));
 if(connect(S,sockaddr_in($p,inet_aton($i)))){
